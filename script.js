f0849b593723ee0b553b61a25a8259a5d45d9693
Buggitt <buggitt@me.com>
2019-01-09 18:24:42 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 
+function redirectMain(){
+
+    // Your application has indicated there's an error
+    window.setTimeout(function(){
+
+        // Move to a new location or you can do something else
+        window.location.href = "http://osu.link";
+
+    }, 5000);
+
+}
